5621687a00e37f97207078dc0cb0d10bd29c1671
Guiral97 <guiiral97@gmail.com>
2022-12-20 14:08:09 -0500

    pass-lastname

--- config/passport.js
+++ config/passport.js
@@ -1,7 +1,7 @@
 const passport = require('passport')
 const passportJwt = require('passport-jwt')
 
-const {KEY_JWT} = process.env
+const { KEY_JWT } = process.env
 const User = require('../models/User')
 
 passport.use(
@@ -10,16 +10,17 @@ passport.use(
             jwtFromRequest: passportJwt.ExtractJwt.fromAuthHeaderAsBearerToken(), // de tipo bearer
             secretOrKey: KEY_JWT //con la clave secreta
         }, //la estrategia devuelve la extraccion en un objeto: jwt_payload
-        async (jwt_payload,done) => {
+        async (jwt_payload, done) => {
             try {
-                let user = await User.findOne({ _id :jwt_payload.id }) //buscamos el usuario
+                let user = await User.findOne({ _id: jwt_payload.id }) //buscamos el usuario
                 if (user) {
                     user = { //este es el objeto user que se "inyecta" al req
                         //aqui es donde protejo los datos del usuario
-                         id: user._id,
+                        id: user._id,
                         name: user.name,
+                        lastName: user.lastName,
                         mail: user.mail,
-                        role: user.role, 
+                        role: user.role,
                         photo: user.photo,
                         country: user.country
                     }
@@ -29,7 +30,7 @@ passport.use(
                 }
             } catch (error) {
                 console.log(error)
-                return done(error,false)
+                return done(error, false)
             }
         }
     )
